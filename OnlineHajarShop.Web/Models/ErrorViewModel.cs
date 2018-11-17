e293ca81c5fb25ebbcdcb5bca2ce50d233d686bb
younesselgueraoui <YOUNESSELGUERAOUI@outlook.com>
2018-11-17 18:02:26 +0100

    Ajoutez des fichiers projet.

---
+++ OnlineHajarShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineHajarShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
